7432dc6c5fe127c664248dfde193983ca29d4cf2
Yuri Shalk <yuri.shalk@hotmail.com>
2026-02-20 14:20:40 -0300

    Desafios extras feitos

--- js/app.js
+++ js/app.js
@@ -5,6 +5,15 @@ limpar();
 function adicionar() {
   let produtoSelecionado = document.getElementById("produto").value;
   let qtyNoCarrinho = document.getElementById("quantidade").value;
+
+  if(qtyNoCarrinho <= 0 || isNaN(qtyNoCarrinho)) {
+    alert("A quantidade deve ser maior que zero.");
+    return;
+  } else if (!produtoSelecionado || produtoSelecionado.trim() === "") {
+    alert("Selecione um produto para adicionar ao carrinho.");
+    return;
+  }
+
   let produtoPrecoTotal = adicionarProdutoNoCarrinho(produtoSelecionado, qtyNoCarrinho);
   calcularValorTotal(produtoPrecoTotal);
   document.getElementById("quantidade").value = 0;
@@ -36,4 +45,40 @@ function limpar() {
   total.innerHTML = "R$ 0,00";
   carrinhoDeProdutos.innerHTML = "";
   valorTotal = 0;
+}
+
+// Atividades extras
+function adicionarCliente() {
+  let nome = document.getElementById("nome").value;
+  let idade = document.getElementById("idade").value;
+  let clienteInfo = document.getElementById("cliente-info");
+
+  alert(`Cliente ${nome} de ${idade} anos cadastrado com sucesso!`);
+  console.log(`Cliente ${nome} de ${idade} anos cadastrado com sucesso!`);
+
+  clienteInfo.textContent = `Cliente: ${nome}, Idade: ${idade} anos`;
+}
+
+function somarNumeros() {
+  let num1 = parseFloat(document.getElementById("primeiro-numero").value);
+  let num2 = parseFloat(document.getElementById("segundo-numero").value);
+  let resultado = num1 + num2;
+
+  console.log(`A soma de ${num1} e ${num2} é: ${resultado}`);
+}
+
+function splitFrase() {
+  let frase = document.getElementById("frase-input").value;
+  let frases = frase.split(";");
+  console.log(`primeira frase: ${frases[0]}, segunda frase: ${frases[1]}`);
+}
+
+function splitNumeros() {
+  let numeros = document.getElementById("input-numeros").value;
+  let numerosArray = numeros.split(",");
+
+  for (let i = 0; i < numerosArray.length; i++) {
+    numerosArray[i] = parseFloat(numerosArray[i].trim());
+    console.log(`Número ${i + 1}: ${numerosArray[i]}`);
+  }
 }
